8a7022105f9a038553c78ce1b0b0811e510e9cfc
mohamedAhmedMokhtarElkomy <mohamed.mokhtar.elkomy@gmail.com>
2023-10-31 14:15:50 +0200

    success message

---
+++ finder-app/finder.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]
+then
+    echo "error there is missing arguments"
+    return 1
+fi
+
+filesdir=$1
+searchstr=$2
+
+if [ -d $filesdir ]
+then
+
+    countFiles=$(find $filesdir | wc -l)
+    # countMatches=$(find $filesdir -name *$searchstr* | wc -l)
+
+    tmpDir="$filesdir/*"
+    countMatches=$(grep -c $searchstr $tmpDir | wc -l)
+
+    countFiles=$((countFiles-1))
+
+    echo "The number of files are $countFiles and the number of matching lines are $countMatches"
+
+else
+    echo "Directory not found"
+    return 1
+fi
---
+++ finder-app/writer.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]
+then
+    echo "error there is missing arguments"
+    return 1
+fi
+
+writefile=$1
+writestr=$2
+
+dir=${writefile%/*} #Delete the shortest match of string from the end:
+
+if ! [ -d $dir ]
+then
+    mkdir -p $dir
+    
+fi
+
+echo $writestr > $writefile 
+
